04f3e3076cae045391fb347eaefcbe5f41b77b82
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-07-15 11:06:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function fibonacci(num) {
-// your code here
-	 if (n <= 1) {
-        return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+// your code her	let fib=[0,1];
+	for(let i=2; i<=num; i++){
+		fib[i]=fib[i-1]+fib[i-2]
+	}
+	return fib[num-1]
 }
 
 module.exports = fibonacci;
